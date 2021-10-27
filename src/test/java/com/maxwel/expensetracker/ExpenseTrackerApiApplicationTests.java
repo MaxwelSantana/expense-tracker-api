7b7d01270721a3a401c12f1c0e5591edf91fb10c
Maxwel <maxwelsant96@icloud.com>
2021-10-27 18:48:24 -0300

    Start spring project

---
+++ src/test/java/com/maxwel/expensetracker/ExpenseTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxwel.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
